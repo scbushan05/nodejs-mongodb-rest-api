e4536d51fc59efe9c1766700565c5db72165876a
Bushan SC <bushan1992@zohomail.in>
2022-12-25 12:28:53 +0530

    section-1 completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Web server is up and running!');
+});
+
+app.listen(3000, (req, res) => {
+    console.log('Webserver is started!');
+});
